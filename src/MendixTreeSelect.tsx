4e1796b931d50a357121def32ce649dd93c285c0
bsgriggs <benjaminsgriggs@gmail.com>
2023-06-05 17:54:21 -0400

    pluggable widget api testing

--- src/MendixTreeSelect.tsx
+++ src/MendixTreeSelect.tsx
@@ -1,21 +1,77 @@
 import { ReactElement, createElement, useCallback, useEffect, useState } from "react";
 import { MendixTreeSelectContainerProps } from "../typings/MendixTreeSelectProps";
 import { TreeSelect } from "antd";
-import { ValueStatus } from "mendix";
+import { ValueStatus, ObjectItem } from "mendix";
 import "./ui/TreeSelect.css";
-import { BaseOptionType } from "antd/es/select";
+import { DefaultOptionType } from "antd/es/select";
 
 export function MendixTreeSelect({
     id,
     name,
-    dataString,
-    setAttribute,
-    tabIndex
+    tabIndex,
+    association,
+    dataSource,
+    objKey,
+    label,
+    parentKey
 }: MendixTreeSelectContainerProps): ReactElement {
-    const [data, setData] = useState<BaseOptionType[]>([]);
-    const [value, setValue] = useState<string[]>();
+    const [data, setData] = useState<DefaultOptionType[]>([]);
+    const [value, setValue] = useState<DefaultOptionType[]>([]);
 
     useEffect(() => {
+        if (dataSource.status === ValueStatus.Available && dataSource.items) {
+            setData(convertToOption(dataSource.items));
+        } else {
+            setData([]);
+        }
+    }, [dataSource]);
+
+    useEffect(() => {
+        if (association.status === ValueStatus.Available) {
+            setValue(convertToOption(association.value as ObjectItem[]));
+        } else {
+            setValue([]);
+        }
+    }, [association]);
+
+    const convertToOption = useCallback((list: ObjectItem[]): DefaultOptionType[] => {
+        let node: DefaultOptionType,
+            roots: DefaultOptionType[] = [];
+        let baseList: DefaultOptionType[] = list.map(obj => {
+            return {
+                label: label.get(obj).value,
+                value: objKey.get(obj).displayValue,
+                parentValue: parentKey.get(obj).displayValue,
+                objectItem: obj,
+                children: []
+            };
+        });
+        for (let i = 0; i < baseList.length; i += 1) {
+            node = baseList[i];
+            if (node.parentValue) {
+                // if you have dangling branches check that map[node.parentId] exists
+                baseList.find(opt => opt.value === node.parentValue)?.children?.push(node);
+            } else {
+                roots.push(node);
+            }
+        }
+        return roots;
+    }, []);
+
+    const onChange = useCallback((newValue: DefaultOptionType[]) => {
+        console.info("selected ant", newValue);
+        /* eslint-disable */
+        // @ts-ignore
+        const selected = data.filter(option => newValue.includes(option.value)).map(option => option.objectItem);
+        console.info("selected mx", selected);
+        association.setValue(selected);
+        /* eslint-enable */
+    }, []);
+
+    console.info("data", data);
+
+    /* JSON version
+        useEffect(() => {
         if (dataString.status === ValueStatus.Available) {
             const newOptions: BaseOptionType[] = JSON.parse(dataString.value);
             setData(newOptions);
@@ -33,6 +89,7 @@ export function MendixTreeSelect({
         console.info("selected", newValue);
         setAttribute.setValue(JSON.stringify(newValue));
     }, [setAttribute]);
+    */
 
     return (
         <TreeSelect
@@ -49,7 +106,7 @@ export function MendixTreeSelect({
             multiple
             treeDefaultExpandAll
             onChange={onChange}
-            treeNodeFilterProp="value"
+            treeNodeFilterProp="label"
             treeCheckable
             showCheckedStrategy={TreeSelect.SHOW_ALL}
         />
--- typings/MendixTreeSelectProps.d.ts
+++ typings/MendixTreeSelectProps.d.ts
@@ -3,19 +3,26 @@
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix UI Content Team
  */
-import { DynamicValue, EditableValue } from "mendix";
+import { ActionValue, ListValue, ListAttributeValue, ListExpressionValue, ReferenceSetValue } from "mendix";
 
 export interface MendixTreeSelectContainerProps {
     name: string;
     tabIndex?: number;
     id: string;
-    dataString: DynamicValue<string>;
-    setAttribute: EditableValue<string>;
+    dataSource: ListValue;
+    label: ListExpressionValue<string>;
+    objKey: ListAttributeValue<string>;
+    parentKey: ListAttributeValue<string>;
+    association: ReferenceSetValue;
+    onSelect?: ActionValue;
 }
 
 export interface MendixTreeSelectPreviewProps {
     readOnly: boolean;
-    dataString: string;
-    setAttribute: string;
+    dataSource: {} | { type: string } | null;
+    label: string;
+    objKey: string;
+    parentKey: string;
+    association: string;
     onSelect: {} | null;
 }
