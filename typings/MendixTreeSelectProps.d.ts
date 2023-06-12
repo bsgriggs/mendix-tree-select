d1e86c35c12e1f1d5f1cc68a6b8dd66d0279c98b
bsgriggs <benjaminsgriggs@gmail.com>
2023-06-12 15:45:04 -0400

    prerelease lint

--- typings/MendixTreeSelectProps.d.ts
+++ typings/MendixTreeSelectProps.d.ts
@@ -3,7 +3,14 @@
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix UI Content Team
  */
-import { DynamicValue, EditableValue, ListValue, ListAttributeValue, ListExpressionValue, ReferenceSetValue } from "mendix";
+import {
+    DynamicValue,
+    EditableValue,
+    ListValue,
+    ListAttributeValue,
+    ListExpressionValue,
+    ReferenceSetValue
+} from "mendix";
 import { Big } from "big.js";
 
 export type InputTypeEnum = "MENDIX" | "JSON";
